0812244268978e8017f893edddaeda2f3c98b664
Serhii Melnyk <serj.melnyk@gmail.com>
2018-12-05 18:53:19 +0200

    add creatPost

--- source/bus/posts/actions.js
+++ source/bus/posts/actions.js
@@ -1,5 +1,5 @@
 //Types
-import { FILL_POSTS, FETCH_POSTS_ASYNC } from './types';
+import { FILL_POSTS, FETCH_POSTS_ASYNC, CREATE_POSTS, CREATE_POST_ASYNC } from './types';
 
 //Instruments
 import { api } from '../../REST';
@@ -8,6 +8,13 @@ export const fillPosts = (posts) => {
     return {
         type:    FILL_POSTS,
         payload: posts,
+    };
+};
+
+export const createPost = (post) => {
+    return {
+        type:    CREATE_POSTS,
+        payload: post,
     };
 };
 
@@ -20,4 +27,15 @@ export const fetchPostsAsync = () => async (dispatch) => {
     const result = await response.json();
 
     dispatch(fillPosts(result.data));
+};
+
+export const createPostAsync = (comment) => async (dispatch) => {
+    dispatch({
+        type: CREATE_POST_ASYNC,
+    });
+
+    const response = await api.posts.create(comment);
+    const result = await response.json();
+
+    dispatch(createPost(result.data));
 };
