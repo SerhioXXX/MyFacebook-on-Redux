ba10d9bc452e487d11c82a753441d0a5f47972c9
Serhii Melnyk <serj.melnyk@gmail.com>
2018-12-31 12:45:09 +0200

    fetchUsers.saga.js

--- source/bus/users/__tests__/fetchUsers.saga.js
+++ source/bus/users/__tests__/fetchUsers.saga.js
@@ -14,41 +14,41 @@ const saga = cloneableGenerator(fetchUsers)(fetchUsersAction);
 let clone = null;
 
 describe("fetchUsers saga", () => {
-  describe("should pass until response received", () => {
-    test('should dispatch "startFetching" action', () => {
-      expect(saga.next().value).toEqual(put(uiActions.startFetching()));
+    describe("should pass until response received", () => {
+        test('should dispatch "startFetching" action', () => {
+            expect(saga.next().value).toEqual(put(uiActions.startFetching()));
+        });
+        test("should call a fetch request", () => {
+            expect(saga.next().value).toEqual(apply(api, api.users.fetch));
+            clone = saga.clone();
+        });
     });
-    test("should call a fetch request", () => {
-      expect(saga.next().value).toEqual(apply(api, api.users.fetch));
-      clone = saga.clone();
-    });
-  });
-  describe("should handle a 400 status response", () => {
-    test("a fetch request should return 400 status response", () => {
-      expect(clone.next(__.fetchResponseFail400).value).toEqual(
-        apply(__.fetchResponseFail400, __.fetchResponseFail400.json)
-      );
-    });
-    test("should contain a response data object", () => {
-      expect(clone.next(__.responseDataFail).value).toEqual(
-        put(uiActions.emitError(__.error, "->fetchUsers worker"))
-      );
-    });
-    test('should dispatch "stopFetching" action', () => {
-      expect(clone.next().value).toEqual(put(uiActions.stopFetching()));
-    });
-    test("should finish", () => {
-      expect(clone.next().done).toBe(true);
-    });
-    describe("should handle a 200 status response", () => {
-      test("a fetch request should return 200 status response data object", () => {
-        expect(saga.next(__.fetchResponseSuccess).value).toEqual(
-          apply(__.fetchResponseSuccess, __.fetchResponseSuccess.json)
-        );
-      });
-      test('should dispatch "fillUsers" action', () => {
-        expect(saga.next(__.responseUsersDataSuccess).value)
-          .toMatchInlineSnapshot(`
+    describe("should handle a 400 status response", () => {
+        test("a fetch request should return 400 status response", () => {
+            expect(clone.next(__.fetchResponseFail400).value).toEqual(
+                apply(__.fetchResponseFail400, __.fetchResponseFail400.json)
+            );
+        });
+        test("should contain a response data object", () => {
+            expect(clone.next(__.responseDataFail).value).toEqual(
+                put(uiActions.emitError(__.error, "->fetchUsers worker"))
+            );
+        });
+        test('should dispatch "stopFetching" action', () => {
+            expect(clone.next().value).toEqual(put(uiActions.stopFetching()));
+        });
+        test("should finish", () => {
+            expect(clone.next().done).toBe(true);
+        });
+        describe("should handle a 200 status response", () => {
+            test("a fetch request should return 200 status response data object", () => {
+                expect(saga.next(__.fetchResponseSuccess).value).toEqual(
+                    apply(__.fetchResponseSuccess, __.fetchResponseSuccess.json)
+                );
+            });
+            test('should dispatch "fillUsers" action', () => {
+                expect(saga.next(__.responseUsersDataSuccess).value)
+                    .toMatchInlineSnapshot(`
 Object {
   "@@redux-saga/IO": true,
   "PUT": Object {
@@ -73,9 +73,9 @@ Object {
   },
 }
 `);
-      });
-      test('should dispatch "stopFetching" action', () => {
-        expect(saga.next().value).toMatchInlineSnapshot(`
+            });
+            test('should dispatch "stopFetching" action', () => {
+                expect(saga.next().value).toMatchInlineSnapshot(`
 Object {
   "@@redux-saga/IO": true,
   "PUT": Object {
@@ -86,10 +86,10 @@ Object {
   },
 }
 `);
-      });
-      test("should finish", () => {
-        expect(saga.next().done).toBe(true);
-      });
+            });
+            test("should finish", () => {
+                expect(saga.next().done).toBe(true);
+            });
+        });
     });
-  });
 });
